b54ccbcee6de3b2a7fe42c16150224078e1e4e1b
jainadijain <jain.adityajpr1990@gmail.com>
2025-01-09 14:35:29 +0530

    - updated style.css [global stylesheet], - updated respective styles for corresponding components, - unlocked default encapsulation mehcanism using ViewEncapsulation to "None", - added :host configs inside component decorator Obj for adding   attributes suh as classes, events, - added code for @HostListener & @HostBinding [commented]

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,12 +1,26 @@
-import { Component, input } from '@angular/core';
+import { Component, HostBinding, HostListener, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   standalone: true,
   imports: [],
   templateUrl: './control.component.html',
-  styleUrl: './control.component.css'
+  styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None,
+  // host: {
+  //   class: 'control',
+  //  '(click)': 'onClick()'
+  // }
 })
 export class ControlComponent {
+  @HostBinding('class') className = 'control';
+  @HostListener('click') onClicked() {
+    console.log('clicked!');
+  }
   label = input.required<string>();
+
+  // for host configs inside component decorator object
+  // onClick() {
+  //   console.log('clicked!');
+  // }
 }
