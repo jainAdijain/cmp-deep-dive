d0fd6e2551d331c5b447163205582ddfde48c145
jainadijain <jain.adityajpr1990@gmail.com>
2025-01-11 13:36:56 +0530

    - added dynamic classes, - added ngOnInit() hook, - added string literal type defined for currentServer, - added elementRef in order to access the component element.

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, HostListener, input, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, HostBinding, HostListener, inject, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -13,14 +13,20 @@ import { Component, HostBinding, HostListener, input, ViewEncapsulation } from '
   // }
 })
 export class ControlComponent {
+  private el = inject(ElementRef);
+
   @HostBinding('class') className = 'control';
   @HostListener('click') onClicked() {
     console.log('clicked!');
+    console.log(this.el);
   }
   label = input.required<string>();
+
 
   // for host configs inside component decorator object
   // onClick() {
   //   console.log('clicked!');
   // }
+
+
 }
